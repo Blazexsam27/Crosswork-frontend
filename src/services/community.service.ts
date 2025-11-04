d4fc672582f2d379b730d7915ec97d03835b945a
Sanju Raj Prasad <43962657+Blazexsam27@users.noreply.github.com>
2025-11-04 22:25:15 +0100

    Added community services

--- src/services/community.service.ts
+++ src/services/community.service.ts
@@ -11,6 +11,45 @@ class CommunityService {
       throw new Error(error);
     }
   };
+
+  getAllCommunities = async () => {
+    try {
+      const response = await axios.get(this.endpoint);
+      return response.data;
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  };
+
+  getCommunityById = async (communityId: string) => {
+    try {
+      const response = await axios.get(`${this.endpoint}/${communityId}`);
+      return response.data;
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  };
+
+  updateCommunity = async (communityId: string, formData: FormData) => {
+    try {
+      const response = await axios.put(
+        `${this.endpoint}/${communityId}`,
+        formData
+      );
+      return response.data;
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  };
+
+  deleteCommunity = async (communityId: string) => {
+    try {
+      const response = await axios.delete(`${this.endpoint}/${communityId}`);
+      return response.data;
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  };
 }
 
 export default new CommunityService();
